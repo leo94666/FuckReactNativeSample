bc877e7227f5a1208b9276e769c728d04ea69d55
leo <719947897@qq.com>
2022-07-07 17:16:17 +0800

    init

---
+++ android/app/src/main/java/com/top/react/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.top.react
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.KeyEvent
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.BuildConfig
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
+
+
+class MainActivity : AppCompatActivity() ,DefaultHardwareBackBtnHandler{
+
+    private var mReactRootView: ReactRootView? = null
+    private var mReactInstanceManager: ReactInstanceManager? = null
+
+
+    private val OVERLAY_PERMISSION_REQ_CODE = 1 // 任写一个值
+
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:$packageName")
+                )
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+            }
+        }
+
+        SoLoader.init(this, false)
+
+        mReactRootView = ReactRootView(this)
+        val packages: List<ReactPackage> = PackageList(application).getPackages()
+
+        // 有一些第三方可能不能自动链接，对于这些包我们可以用下面的方式手动添加进来：
+        // packages.add(new MyReactNativePackage());
+        // 同时需要手动把他们添加到`settings.gradle`和 `app/build.gradle`配置文件中。
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackages(packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+        // 注意这里的MyReactNativeApp 必须对应"index.js"中的
+        // "AppRegistry.registerComponent()"的第一个参数
+        // 注意这里的MyReactNativeApp 必须对应"index.js"中的
+        // "AppRegistry.registerComponent()"的第一个参数
+        mReactRootView?.startReactApplication(mReactInstanceManager, "FuckReactNativeSample", null)
+
+        setContentView(mReactRootView)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    // SYSTEM_ALERT_WINDOW permission not granted
+                }
+            }
+        }
+        mReactInstanceManager?.onActivityResult(this, requestCode, resultCode, data)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostPause(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostResume(this, this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostDestroy(this)
+        }
+        if (mReactRootView != null) {
+            mReactRootView!!.unmountReactApplication()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onBackPressed()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager!!.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+}
