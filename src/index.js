81d91da619855601e52a2436f60bfd49e240da07
　”liyang“ <liyang.ok@outlook.com>
2022-07-11 07:38:30 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+const RootStack = createStackNavigator({
+    
+});
